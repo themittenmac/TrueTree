12ab12a2fc6d4532b638b53d6c28c724324f7192
Jaron Bradley <jaron.bradley@gmail.com>
2022-03-11 12:05:02 -0500

    Updated submitted by pid lookup to use Application Services API

--- Src/launchdXPC/launchdXPC.m
+++ Src/launchdXPC/launchdXPC.m
@@ -5,18 +5,50 @@
 //
 
 #include <stdio.h>
+#import <dlfcn.h>
 #import "launchdXPC.h"
 #import <Foundation/Foundation.h>
+#import <ApplicationServices/ApplicationServices.h>
 
+NSDictionary * infoForPID(pid_t pid)
+{
+    NSDictionary *ret = nil;
+    ProcessSerialNumber psn = { 0, 0 };
+    if (GetProcessForPID(pid, &psn) == noErr) {
+        CFDictionaryRef cfDict = ProcessInformationCopyDictionary(&psn,kProcessDictionaryIncludeAllInformationMask);
+        ret = [NSDictionary dictionaryWithDictionary:(__bridge NSDictionary *)cfDict];
+        CFRelease(cfDict);
+    }
+    return ret;
+}
 
-#define ROUTINE_DUMP_PROCESS    0x2c4    // 708
+int getSubmittedPid(int pid) {
+    NSDictionary* info =  infoForPID(pid);
+    //NSLog(@"info: %@", info);
+        
+    long long temp = [[info objectForKey:@"ParentPSN"] longLongValue];
+    long long hi = (temp >> 32) & 0x00000000FFFFFFFFLL;
+    long long lo = (temp >> 0) & 0x00000000FFFFFFFFLL;
+    ProcessSerialNumber parentPSN = {(unsigned long)hi, (unsigned long)lo};
+        
+    NSDictionary* parentDict = (__bridge NSDictionary*)ProcessInformationCopyDictionary (&parentPSN, kProcessDictionaryIncludeAllInformationMask);
+    //NSLog(@"real parent info: %@", parentDict);
+    //NSLog(@"real parent pid: %@", parentDict[@"pid"]);
+        
+    pid_t p = 0;
+    GetProcessPID(&parentPSN, &p);
+    //NSLog(@"real parent pid: %d", p);
+    
+    return p;
+}
 
-extern char *xpc_strerror (int);
+#define ROUTINE_DUMP_PROCESS  0x2c4
 
+//function definition
 NSMutableDictionary* parse(NSString* data);
 
 //hit up launchd (via XPC) to get process info
-NSDictionary* getProcessInfo(unsigned long pid)
+NSString* getSubmittedByPlist(unsigned long pid)
 {
     //proc info
     NSDictionary* processInfo = nil;
@@ -25,13 +57,21 @@ NSDictionary* getProcessInfo(unsigned long pid)
     // passed to launchd to get proc info
     xpc_object_t procInfoRequest = NULL;
     
+    //shared memory for XPC
+    xpc_object_t sharedMemory = NULL;
+    
     //xpc (out) dictionary
     // don't really contain response, but will have (any) errors
     xpc_object_t __autoreleasing response = NULL;
     
-    //pipe to file descriptors
-    // launchd expects a writeable fd for 'passing back' data
-    NSPipe *pipe = nil;
+    //result
+    int result = 0;
+    
+    //dylib handle
+    void *handle = NULL;
+    
+    //function pointer to 'xpc_pipe_interface_routine'
+    static int(*xpc_pipe_interface_routine_FP)(xpc_pipe_t, int, xpc_object_t, xpc_object_t*, int) = NULL;
     
     //(xpc) error
     int64_t xpcError = 0;
@@ -42,46 +82,88 @@ NSDictionary* getProcessInfo(unsigned long pid)
     //init dictionary
     procInfoRequest = xpc_dictionary_create(NULL,NULL,0);
 
-    //init pipe
-    pipe = [NSPipe pipe];
-
-    //specify xpc routine
-    // 0x2c4 is what dumps the process info
-    xpc_dictionary_set_uint64(procInfoRequest, "routine", ROUTINE_DUMP_PROCESS);
+    //init buffer
+    // size from reversing launchctl
+    size_t processInfoLength = 0x100000;
+    vm_address_t processInfoBuffer = 0;
     
-    //specify xpc subsystem
-    xpc_dictionary_set_uint64 (procInfoRequest, "subsystem", 2);
-
-    //specify the file descriptor
-    xpc_dictionary_set_fd(procInfoRequest, "fd", pipe.fileHandleForWriting.fileDescriptor);
-
-    //specify (targert) process id
+    //bytes written via XPC call
+    uint64_t bytesWritten = 0;
+    
+    //alloc buffer
+    if(noErr != vm_allocate(mach_task_self(), &processInfoBuffer, processInfoLength, 0xf0000003))
+    {
+        //bail
+        goto bail;
+    }
+    
+    sharedMemory = xpc_shmem_create((void*)processInfoBuffer, processInfoLength);
+    if(0 == sharedMemory)
+    {
+        //bail
+        goto bail;
+    }
+    
+    //add pid to request
     xpc_dictionary_set_int64(procInfoRequest, "pid", pid);
     
-    //grab from
-    globalData  = (struct xpc_global_data *) _os_alloc_once_table[1].ptr;
-
+    //add shared memory object
+    xpc_dictionary_set_value(procInfoRequest, "shmem", sharedMemory);
+    
+    //grab from global data structure
+    // contains XPC bootstrap pipe (launchd)
+    globalData = (struct xpc_global_data *)_os_alloc_once_table[1].ptr;
+    
+    //open XPC lib
+    handle = dlopen("/usr/lib/system/libxpc.dylib", RTLD_LAZY);
+    if(NULL == handle)
+    {
+        //bail
+        goto bail;
+    }
+    
+    xpc_pipe_interface_routine_FP = dlsym(handle, "_xpc_pipe_interface_routine");
+    if(NULL == xpc_pipe_interface_routine_FP)
+    {
+        //bail
+        goto bail;
+    }
+    
     //request process info
-    if(0 != xpc_pipe_routine((__bridge xpc_pipe_t)(globalData->xpc_bootstrap_pipe), procInfoRequest, &response))
+    result = xpc_pipe_interface_routine_FP((__bridge xpc_pipe_t)(globalData->xpc_bootstrap_pipe), ROUTINE_DUMP_PROCESS, procInfoRequest, &response, 0x0);
+    if(0 != result)
     {
         //error
         goto bail;
     }
     
-    //check for other error
+    //check for other error(s)
     xpcError = xpc_dictionary_get_int64(response, "error");
     if(0 != xpcError)
     {
         //error
+        //printf("error: %llx\n", xpcError);
         goto bail;
     }
     
+    //get number of bytes written (to shared memory)
+    bytesWritten = xpc_dictionary_get_uint64(response, "bytes-written");
+    
     //parse
-    processInfo = parse([[NSString alloc] initWithData:[pipe.fileHandleForReading availableData] encoding:NSUTF8StringEncoding]);
+    processInfo = parse([[NSString alloc] initWithBytes:(const void *)processInfoBuffer length:bytesWritten encoding:NSUTF8StringEncoding]);
     
 bail:
     
-    return processInfo;
+    //free buffer
+    if(0 != processInfoBuffer)
+    {
+        //free
+        vm_deallocate(mach_task_self(), processInfoBuffer, processInfoLength);
+        processInfoBuffer = 0;
+    }
+    
+    return processInfo[@"path"];
+    //return processInfo;
 }
 
 
