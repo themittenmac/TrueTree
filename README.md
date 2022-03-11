6b9f3981c4302ba40b7128cd9bc76eff711f8a01
Jaron Bradley <jaron.bradley@gmail.com>
2022-03-11 11:39:00 -0500

    Many major updates to fix various issues

---
+++ README.md
@@ -0,0 +1,35 @@
+#  TrueTree
+
+<p align="center"><img src="https://themittenmac.com/wp-content/uploads/2020/02/Frame-5-scaled.jpg"></p>
+
+**About** \
+Read all about the TrueTree concept [here](https://themittenmac.com/the-truetree-concept/)
+
+TrueTree is more than just a pstree command for macOS. It is used to display a process tree for current running processes while using a hierarchy built on additoinal pids that can be collected from the operating system.  The standard process tree on macOS that can be built with traditional pids and ppids is less than helpful on macOS due to all the XPC communication at play.  The vast majority of processes end up having a parent process of launchd. TrueTree however displays a process tree that is meant to be useful to incident responders, threat hunters, researchers, and everything in between!  
+
+
+**./TrueTree -h** \
+--nocolor -> Do not color code items in output
+--notree  -> Do not print tree format. Just print in list format
+--timestamps -> Include process timestamps
+--standard -> Print the standard Unix tree instead of TrueTree
+--sources -> Print the source of where each processes parent came from
+--version -> Print the TrueTree version number
+-o <filename> -> output to file
+
+<span style="color:blue">Note: Requires Root</span>
+
+**./TrueTree** \
+Displays an enhanced process tree using the TrueTree concept
+<p align="center"><img src="https://themittenmac.com/wp-content/uploads/2020/02/bigger_preview_photo.png"></p>
+
+<br/><br/>
+
+**./TrueTree --standard** \
+For tree output based on standard pids and ppids use --standard
+<p align="center"><img src="https://themittenmac.com/wp-content/uploads/2020/03/tt_standard.png"></p>
+<br/><br/>
+
+**./TrueTree --timestamps** \
+For output in either format with process create time added use the --timestamps option
+<p align="center"><img src="https://themittenmac.com/wp-content/uploads/2020/03/tt_timestamps.png"></p>
---
+++ Src/args.swift
@@ -0,0 +1,68 @@
+//
+//  args.swift
+//  TrueTree
+//
+//  Created by Jaron Bradley on 2/20/20.
+//  2020 TheMittenMac
+//
+
+import Foundation
+
+
+class ArgManager {
+    
+    var color = true
+    var timestamps = false
+    var standardMode = false
+    var sources = false
+    var treeMode = true
+    var toFile: String?
+    let availableArgs = ["--nocolor", "--notree", "--timestamps", "-o", "--standard", "--version", "--sources"]
+    
+    init(suppliedArgs: [String]) {
+        setArgs(suppliedArgs)
+    }
+    
+    func setArgs(_ args:[String]) {
+        for (x,arg) in (args).enumerated() {
+            if x == 0 || !arg.starts(with: "-") {
+                continue
+            } else if arg == "-h" || arg == "-help" {
+                self.printHelp()
+            }else if arg == "--nocolor" {
+                color.toggle()
+            } else if arg == "--timestamps" {
+                timestamps.toggle()
+            } else if arg == "--standard" {
+                standardMode.toggle()
+            } else if arg == "--sources" {
+                sources.toggle()
+            } else if arg == "--notree" {
+                treeMode.toggle()
+            } else if arg == "--version" {
+                print(version)
+                exit(1)
+            } else if arg == "-o" {
+                if args.count > x+1 && !availableArgs.contains(args[x+1]) {
+                    toFile = args[x+1]
+                } else {
+                    toFile = "tree_output.txt"
+                }
+            } else {
+                print("Unidentified argument " + arg)
+                exit(1)
+            }
+        }
+    }
+    
+    func printHelp() {
+        print("--nocolor -> Do not color code items in output")
+        print("--notree  -> Do not print tree format. Just print in list format")
+        print("--timestamps -> Include process timestamps")
+        print("--standard -> Print the standard Unix tree instead of TrueTree")
+        print("--sources -> Print the source of where each processes parent came from")
+        print("--version -> Print the TrueTree version number")
+        print("-o <filename> -> output to file")
+        exit(1)
+    }
+}
